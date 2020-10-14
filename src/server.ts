18851e793f8ece85ca7496a347b5d8cb661be580
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-10-13 23:40:08 -0300

    criando um orfanato e configurando o banco de dados.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,35 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanages from './models/Orphanage';
+import './database/connection';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = req.body;
+  const orphanagesRepository = getRepository(Orphanages);
+  const orphanage = await orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+  await orphanagesRepository.save(orphanage);
+  res.json(orphanage);
+});
 
 app.listen(3333, () => {
   console.log('Aplicacao rodando na porta 3333');
